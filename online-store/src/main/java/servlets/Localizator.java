a92cb86a4d3163fb6386b8e89095db595f452a1e
baradist <baradist@ya.ru>
2016-05-10 02:31:21 +0300

    log4j + lombok

--- online-store/src/main/java/servlets/Localizator.java
+++ online-store/src/main/java/servlets/Localizator.java
@@ -1,5 +1,7 @@
 package servlets;
 
+import lombok.extern.log4j.Log4j;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,14 +9,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+@Log4j
 @WebServlet("/localizator")
 public class Localizator extends HttpServlet {
+//    private static final Logger log = Logger. getRootLogger();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        request.getSession(true).setAttribute("local", request.getParameter("local"));
+        String local = request.getParameter("local");
+        request.getSession(true).setAttribute("local", local);
+
+        log.info("Locale set to " + local);
         // TODO:
 //        request.getRequestDispatcher("/").forward(request, response);
         response.sendRedirect("/");
